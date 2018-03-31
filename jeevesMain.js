3df19223c27b23de59e35751a9885429f573a56b
floon <nader.kirrish@gmail.com>
2018-03-31 16:23:25 -0400

    command boilerplate

--- jeevesMain.js
+++ jeevesMain.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const firebase = require('firebase');
 
-import * as firebase from 'firebase';
 const database = firebase.initializeApp({
     apiKey: "AIzaSyDjmBGDSf92A10zLJBtZt-wOX_EbTr79Us",
     authDomain: "jeeves-7facd.firebaseapp.com",
@@ -9,17 +9,33 @@ const database = firebase.initializeApp({
 });
 
 const prefix = '!';
+const categories = []
 
 client.on('ready', () => {
     console.log(`logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === `${prefix}ping`) {
-        msg.reply('pong')
-    }
+    if (msg.content.startsWith(`${prefix}`)) {
+        let strings = msg.content.split(' ');
 
+        switch(strings[0]) {
+            case `${prefix}ping`:
+                msg.reply('pong');
+                break;
+            case `${prefix}add`:
+                break;
+            case `${prefix}delete`:
+                break;
+            case `${prefix}help` :
+                break;
+            case `${prefix}list`:
+                break;
+        }
+    }
     //insertion for commands is here
 });
 
 client.login('NDI5NzIwNjY2OTA5MDQ4ODMy.DaFwig.JkuqQ-J8KROY1hkLJUZWaK-3Qak');
+
+//Helpers
