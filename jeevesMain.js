ae334488decb9f2b70a548d1d301970240070cf7
floon <nader.kirrish@gmail.com>
2018-03-31 18:26:02 -0400

    categories

--- jeevesMain.js
+++ jeevesMain.js
@@ -9,7 +9,7 @@ const database = firebase.initializeApp({
 });
 
 const prefix = '!';
-const categories = [];
+const categories = ['photography', 'video', 'streaming', 'music-production', 'web-dev', 'social-outreach', 'game-dev'];
 
 client.on('ready', () => {
     database.auth().signInWithEmailAndPassword("jeeves@jeeves.com", "jeeves12").catch((error => {console.log(error.message)}));
@@ -25,11 +25,13 @@ client.on('message', msg => {
                 msg.reply('pong');
                 break;
             case `${prefix}add`:
-                //TODO param count checks and category validation
+                //Check # of params and check for valid category
+                if (strings.length === 4 && )
                 database.database().ref(`users/${msg.author.id}/${strings[1]}`).set({
                     name: strings[2],
                     url: strings [3]
                 });
+                //TODO add category role
                 break;
             case `${prefix}delete`:
                 msg.reply('not implemented yet');
@@ -42,9 +44,12 @@ client.on('message', msg => {
                 break;
         }
     }
-    //insertion for commands is here
 });
 
 client.login('NDI5NzIwNjY2OTA5MDQ4ODMy.DaFwig.JkuqQ-J8KROY1hkLJUZWaK-3Qak');
 
 //Helpers
+
+let validCategory = function (category) {
+    categories.forEach
+};
