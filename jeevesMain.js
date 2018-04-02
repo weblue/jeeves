3fbf3c2c4ae06959e9d5f9dd70eb754a9015846b
floon <nader.kirrish@gmail.com>
2018-04-02 00:50:47 -0400

    Linter

--- jeevesMain.js
+++ jeevesMain.js
@@ -1,135 +1,143 @@
-//Imports
+// Imports
 const Discord = require('discord.js');
 const firebase = require('firebase');
 const fs = require('fs');
 
-//Config
+const client = new Discord.Client();
+
+// Config
 const { prefix, firebasetoken, discordtoken } = require('./config.json');
 
-//Commands
+// Commands
 const commandFiles = fs.readdirSync('./commands');
 commandFiles.forEach((ele) => {
-    const command = require(`./commands/${ele}`);
+  const command = require(`./commands/${ele}`);
 
-    client.commands.set(command.name, command);
+  client.commands.set(command.name, command);
 });
-
-//Database
-const database = firebase.initializeApp({
-    apiKey: firebasetoken,
-    authDomain: "jeeves-7facd.firebaseapp.com",
-    databaseURL: "https://jeeves-7facd.firebaseio.com/"
-});
-
-//Constants
-const categories = ['photography', 'video', 'streaming', 'music-production',
-                    'web-dev', 'social-outreach', 'game-dev', 'programming'];
-const roleMap = {
-    'photography': 'Photography',
-    'video': 'Video Production',
-    'web-dev': 'Web Development',
-    'music-production': 'Music Production',
-    'programming': 'Programming',
-    'streaming': 'Streaming',
-    'social-outreach': 'Social Outreach/Branding',
-    'game-dev': 'Video Game Development'
-};
-
-//Bot Client
-const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-//Listeners
+
+// Database
+const database = firebase.initializeApp({
+  apiKey: firebasetoken,
+  authDomain: 'jeeves-7facd.firebaseapp.com',
+  databaseURL: 'https://jeeves-7facd.firebaseio.com/',
+});
+
+// Constants
+const categories = ['photography', 'video', 'streaming', 'music-production',
+  'web-dev', 'social-outreach', 'game-dev', 'programming'];
+const roleMap = {
+  'photography': 'Photography',
+  'video': 'Video Production',
+  'web-dev': 'Web Development',
+  'music-production': 'Music Production',
+  'programming': 'Programming',
+  'streaming': 'Streaming',
+  'social-outreach': 'Social Outreach/Branding',
+  'game-dev': 'Video Game Development',
+};
+
+// Helpers
+const errorMessages =
+  ['Please stop; you\'re killing me.',
+    'Error with your input!',
+    'What the hell are you doing?',
+  ];
+
+function randomErrorMessage() {
+  const index = Math.floor(Math.random() * errorMessages.length);
+  return errorMessages[index];
+}
+
+// Listeners
 client.on('ready', () => {
-    database.auth().signInWithEmailAndPassword("jeeves@jeeves.com", "jeeves12").catch((error => {
-        console.log(error.message)
-    }));
-    console.log(`logged in as ${client.user.tag}!`);
+  database.auth().signInWithEmailAndPassword('jeeves@jeeves.com', 'jeeves12').catch(((error) => {
+    console.log(error.message);
+  }));
+  console.log(`logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+client.on('message', (msg) => {
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
-    console.log(`Processing: ${msg}`);
+  console.log(`Processing: ${msg}`);
 
-    let args = msg.content.slice(prefix.length).split(/ +/);
-    const cmd = args.shift().toLowerCase();
+  const args = msg.content.slice(prefix.length).split(/ +/);
+  const cmd = args.shift().toLowerCase();
 
-    if (!client.commands.has(cmd)) return msg.reply(`${randomErrorMessage()} That's not a command!`);
+  if (!client.commands.has(cmd)) {
+    msg.reply(`${randomErrorMessage()} That's not a command!`);
+  } else {
     try {
-        client.commands.get(cmd).execute(message, args);
+      client.commands.get(cmd).execute(msg, args);
     } catch (error) {
-        console.error(msg.author + 'triggered a command_exec_error: ' + error);
-        msg.author.send(randomErrorMessage() + error);
+      console.error(`${msg.author}triggered a command_exec_error: ${error}`);
+      msg.author.send(randomErrorMessage() + error);
     }
-        // case `help` :
-        //     msg.reply('not implemented yet');
-        // case `invite`:
-        //     msg.reply('not implemented yet');
+  }
+  // case `help` :
+  //     msg.reply('not implemented yet');
+  // case `invite`:
+  //     msg.reply('not implemented yet');
 });
 
-client.login(discordtoken).then((token) => {console.debug(token)});
+client.login(discordtoken).then((token) => {
+  console.debug(token);
+});
 
-//Helpers
+// Helpers
 
-let validCategory = function (category) {
-    console.log('validating category: ' + category);
-    if (category === 'Moderator') {
-        console.log('This fucker just tried to add a moderator project');
-        return null;
-    }
-    for (let i = 0; i < categories.length; i++) {
-        const element = categories[i];
-        if (category.toLowerCase() === element.toLowerCase()) {
-            return element;
-        }
-    }
+function validCategory(category) {
+  console.log(`validating category: ${category}`);
+  if (category === 'Moderator') {
+    console.log('This fucker just tried to add a moderator project');
     return null;
-};
-
-let getUserPath = function (user) {
-    return `users/${user}`;
-};
-
-let getCategoryPath = function (userPath, category) {
-    return `${userPath}/${category}`;
-};
-
-let getProjectPath = function (categoryPath, projectName) {
-    return `${categoryPath}/${projectName}`;
-};
-
-const errorMessages = ['Please stop; you\'re killing me.', 'Error with your input!', 'What the hell are you doing?'
-];
-
-let randomErrorMessage = function () {
-    let index = Math.floor(Math.random() * errorMessages.length);
-    return errorMessages[index];
-};
-
-let findRole = function (role) {
-    let roles = client.guilds.array()[0].roles.array();
-    let found = null;
-    roles.forEach((element) => {
-        if (element.name === roleMap[role]) {
-            found = element;
-        }
-    });
-    if (found === null) {
-        console.log('Error: role not found');
-        return null;
-    } else {
-        return found;
+  }
+  for (let i = 0; i < categories.length; i += 1) {
+    const element = categories[i];
+    if (category.toLowerCase() === element.toLowerCase()) {
+      return element;
     }
-};
+  }
+  return null;
+}
 
+function getUserPath(user) {
+  return `users/${user}`;
+}
+
+function getCategoryPath(userPath, category) {
+  return `${userPath}/${category}`;
+}
+
+function getProjectPath(categoryPath, projectName) {
+  return `${categoryPath}/${projectName}`;
+}
+
+
+function findRole(role) {
+  const roles = client.guilds.array()[0].roles.array();
+  let found = null;
+  roles.forEach((element) => {
+    if (element.name === roleMap[role]) {
+      found = element;
+    }
+  });
+  if (found === null) {
+    console.log('Error: role not found');
+    return null;
+  }
+  return found;
+}
 
 module.exports = {
-    prefix,
-    findRole,
-    randomErrorMessage,
-    getProjectPath,
-    validCategory,
-    getUserPath,
-    getCategoryPath
+  prefix,
+  findRole,
+  getProjectPath,
+  validCategory,
+  getUserPath,
+  getCategoryPath,
+  database,
 };
