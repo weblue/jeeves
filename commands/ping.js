d524b03412177a0d134f9e3944fa84dc17af961b
floon <nader.kirrish@gmail.com>
2018-04-02 00:50:47 -0400

    Linter

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 const main = require('../jeevesMain');
 
 module.exports = {
-    name: 'ping',
-    description: 'Ping!',
-    usage: `${main.prefix}ping`,
-    execute(msg) {
-        msg.reply('pong');
-        console.log(`Sent message: pong to ${msg.author.username}`);
-    },
+  name: 'ping',
+  description: 'Ping!',
+  usage: `${main.prefix}ping`,
+  execute(msg) {
+    msg.reply('pong');
+    console.log(`Sent message: pong to ${msg.author.username}`);
+  },
 };
